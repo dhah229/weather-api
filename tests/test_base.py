5c059100227e1e92ae63e7a47fd40ffdfb74ceca
David Hah <uhah229@gmail.com>
2024-09-08 09:49:04 -0400

    added more tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -9,7 +9,7 @@ from weather_api.base import GeoMetAPI
 start_date = datetime(2020, 1, 1)
 end_date = datetime(2020, 3, 1)
 hydrometric_station = "02GA018"
-weather_station = "6158350"
+weather_station = "6158355"
 
 
 def url(accessor: GeoMetAPI, station: str):
@@ -25,13 +25,13 @@ def metadata(accessor: GeoMetAPI, station: str):
 
 
 def dataframe(accessor: GeoMetAPI, station: str):
-    wa: GeoMetAPI = accessor(stn_id=station)
+    wa: GeoMetAPI = accessor(stn_id=station, start_date=start_date, end_date=end_date)
     dcf = wa.to_dict_frame()
     assert isinstance(dcf[station], pd.DataFrame)
 
 
 def xarray(accessor: GeoMetAPI, station: str):
-    wa: GeoMetAPI = accessor(stn_id=station)
+    wa: GeoMetAPI = accessor(stn_id=station, start_date=start_date, end_date=end_date)
     ds = wa.to_xr()
     assert isinstance(ds, xr.Dataset)
 
@@ -58,3 +58,25 @@ def test_weather_dataframe():
 
 def test_hydrometric_dataframe():
     dataframe(accessor=HydrometricStations, station=hydrometric_station)
+
+
+def test_weather_xarray():
+    xarray(accessor=WeatherStations, station=weather_station)
+
+
+def test_hydrometric_xarray():
+    xarray(accessor=HydrometricStations, station=hydrometric_station)
+
+
+def test_hydrometric_realtime():
+    wa = HydrometricStations(stn_id=hydrometric_station, realtime=True)
+    ds = wa.to_xr()
+    assert isinstance(ds, xr.Dataset)
+
+
+def test_weather_hourly():
+    wa = WeatherStations(
+        stn_id=weather_station, hourly=True, start_date=start_date, end_date=end_date
+    )
+    ds = wa.to_xr()
+    assert isinstance(ds, xr.Dataset)
