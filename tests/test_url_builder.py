a2e50d941a9f8bfa276c79839b4e48e87318e170
David Hah <dhah@aquanty.com>
2024-09-10 11:09:53 -0400

    more tests

--- tests/test_url_builder.py
+++ tests/test_url_builder.py
@@ -11,3 +11,33 @@ def test_date_range():
     ub = UrlBuilder(route=route)
     ub.date_range = (start_date, end_date)
     assert ub.date_range == "2019-01-01 00:00:00/2019-02-01 00:00:00"
+
+
+def test_setter_getter():
+    ub = UrlBuilder(route=route)
+    ub.date_range_hydrometric = (start_date, end_date)
+    assert ub.date_range_hydrometric == "2019-01-01/2019-02-01"
+
+    ub.sortby = "DATE"
+    assert ub.sortby == "DATE"
+
+    ub.format = "csv"
+    assert ub.format == "csv"
+
+    ub.limit = 10
+    assert ub.limit == 10
+
+    ub.startindex = 10
+    assert ub.startindex == 10
+
+    ub.bbox = [1, 1, 1, 1]
+    assert ub.bbox == "1,1,1,1"
+
+    ub.climate_identifier = 1
+    assert ub.climate_identifier == 1
+
+    ub.station_number = "TEST1234"
+    assert ub.station_number == "TEST1234"
+
+    ub.properties = "test"
+    assert ub.properties == "test"
